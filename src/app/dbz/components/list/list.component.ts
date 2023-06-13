f5eff2d21dc1d27012679ed82f2c745b2cba4bb4
iscgama <iscgama@gmail.com>
2023-06-13 09:27:33 -0600

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter( id: string ):void {
+    // TODO: emitir el ID del personaje
+    this.onDelete.emit( id );
+
+  }
+}
